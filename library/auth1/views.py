48024b0fdf7a29475c7136c91f70d96cf0266131
debian-master <chavanhchetan@gmail.com>
2022-04-07 19:35:46 +0530

    redirecting user after sign up to list of books

--- library/auth1/views.py
+++ library/auth1/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
@@ -12,3 +13,6 @@ class SignupView(generics.CreateAPIView):
     serializer_class = SignupSerializer
     permission_classes = (AllowAny, )
 
+    def post(self, request, *args, **kwargs):
+        super().post(request,*args, **kwargs)
+        return redirect('book_listing')
